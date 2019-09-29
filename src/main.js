3feedd9f671525a72c29d2d0054d6fd045b1092c
jbentleyrsi <josh.bentley@ruralsourcing.com>
2019-09-29 18:18:52 -0500

    fixed scraper for months without events

--- src/main.js
+++ src/main.js
@@ -3,22 +3,26 @@ const coxScraper = require("./scrapers/coxScraper");
 const logger = require("./util/logger");
 const saveToJson = require("./util/saveToJson");
 
-async function main() {
+const months = 3;
+
+function main() {
+  runScraper()
+    .then(data => {
+      saveToJson("../events.json", { events: data }, true);
+      logger(`done`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+async function runScraper() {
   try {
     let data = [];
-    data.push(...(await chesapeakeScraper(3, true)));
-    data.push(...(await coxScraper(3, true)));
+    data.push(...(await chesapeakeScraper(months)));
+    data.push(...(await coxScraper(months)));
     return data;
   } catch (err) {
     console.error(err);
   }
 }
-
-main()
-  .then(data => {
-    saveToJson("../events.json", { events: data }, true);
-    logger(`done`);
-  })
-  .catch(err => {
-    console.error(err);
-  });
